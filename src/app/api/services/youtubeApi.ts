c16798795e3f7d48c32b7fe0d215cd867c8d7140
nircoren <nirappleonly@gmail.com>
2024-05-21 17:51:23 +0300

    commented unneccessary line.

--- src/app/api/services/youtubeApi.ts
+++ src/app/api/services/youtubeApi.ts
@@ -5,7 +5,7 @@ import { api } from "../configs/axiosConfigs";
 // const youtubeEndpoint = '/api/youtube'
 const apiKey = process.env.YOUTUBE_API_KEY;
 const maxResults = 20;
-let apiUrl = `https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&maxResults=${maxResults}&order=relevance&part=snippet&videoId=${video_id}`;
+// let apiUrl = `https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&maxResults=${maxResults}&order=relevance&part=snippet&videoId=${video_id}`;
 
 export const youtubeApi = {
   user: async function (cancel = false) {
