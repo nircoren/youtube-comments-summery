fff02e1e7ff38d5df2de3eb634d90221b9c0b1fd
nircoren <nirappleonly@gmail.com>
2024-05-23 15:21:52 +0300

    added test insert user

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,14 +2,39 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../lib/prisma'
 
+function validateUser() {
+
+}
+
 export async function GET() {
     const users = await prisma.user.findMany()
-    console.log(users)
     return NextResponse.json({ props: { users } });
 }
 
 export async function POST() {
-    const users = await prisma.user.findMany()
-    console.log(users)
-    return NextResponse.json({ props: { users } });
+    const user = _testInsertUser()
+    return NextResponse.json({ props: { user } });
+}
+
+async function _testInsertUser() {
+    const email ='wawa@gmail.com'
+    const name = 'nirco';
+    if (!email || !name) {
+      throw new Error("Email or name undefined.");
+    }
+    // Find the user in the database
+    const User = await prisma.user.upsert({
+      where: {
+        email,
+      },
+      update: {},
+      create: {
+        email,
+        name,
+        props: {
+          create: {
+          },
+        },
+      },
+    });
 }
